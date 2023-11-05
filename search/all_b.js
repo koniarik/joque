ace2beb4c94f612a4c861204cfcd33f69f0a40bf
koniarik <5853442+koniarik@users.noreply.github.com>
2023-11-05 19:56:28 +0000

    Deploying to gh-pages from  @ 2fbb7b16702b2fccb53e3447e966930aede333ef 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['operator_3d_39',['operator=',['../classjoque_1_1exec__coro.html#a522a2e01000a50094fd1bbd36b543b91',1,'joque::exec_coro::operator=()'],['../structjoque_1_1job__ptr.html#a72d27da3dc94e971d56eb62bcc026022',1,'joque::job_ptr::operator=(const job_ptr &amp;) noexcept=delete'],['../structjoque_1_1job__ptr.html#a4a1e86adfac78e692c4684207faf8e2b',1,'joque::job_ptr::operator=(job_ptr &amp;&amp;) noexcept=default']]],
+  ['operator_3d_39',['operator=',['../classjoque_1_1exec__coro.html#a522a2e01000a50094fd1bbd36b543b91',1,'joque::exec_coro']]],
   ['out_40',['out',['../structjoque_1_1out.html',1,'joque::out'],['../structjoque_1_1out.html#a1f620c3d5f8e8e00423082c16cca519d',1,'joque::out::out()']]],
   ['out_2ehpp_41',['out.hpp',['../out_8hpp.html',1,'']]],
   ['out_5ftag_42',['out_tag',['../structjoque_1_1out__tag.html',1,'joque']]],
--- structjoque_1_1job__ptr.js
+++ structjoque_1_1job__ptr.js
@@ -1,9 +1,5 @@
 var structjoque_1_1job__ptr =
 [
     [ "job_ptr", "structjoque_1_1job__ptr.html#a9db05b0f4f76eca2c19963d063e25a4c", null ],
-    [ "job_ptr", "structjoque_1_1job__ptr.html#a8d2a4465482b2535f331176f8a6922f9", null ],
-    [ "job_ptr", "structjoque_1_1job__ptr.html#a6660f07338caf7846a0ab4a401c5251a", null ],
-    [ "job_ptr", "structjoque_1_1job__ptr.html#a8c93d78dd0a95c6a7e10d807a3c8e6be", null ],
-    [ "operator=", "structjoque_1_1job__ptr.html#a72d27da3dc94e971d56eb62bcc026022", null ],
-    [ "operator=", "structjoque_1_1job__ptr.html#a4a1e86adfac78e692c4684207faf8e2b", null ]
+    [ "job_ptr", "structjoque_1_1job__ptr.html#a8d2a4465482b2535f331176f8a6922f9", null ]
 ];
