e05f2e5e19a2a4c7c1b46df47fc43b3a7aad7ce9
Jan Veverak Koniarik <squirrelcze@gmail.com>
2024-03-19 21:34:35 +0100

    minor fmt fix

--- include/joque/exec_visitor.hpp
+++ include/joque/exec_visitor.hpp
@@ -12,14 +12,24 @@ class exec_visitor
 {
 public:
         /// Executed before node is enqued into internal queue of execution.
-        virtual void on_node_enque( const dag_node& ){};
+        virtual void on_node_enque( const dag_node& )
+        {
+        }
 
         /// Executed once execution starts for the node.
-        virtual void on_run_start( const dag_node& ){};
+        virtual void on_run_start( const dag_node& )
+        {
+        }
 
         /// Executed after execution of node.
         /// \param rec If run produces a run record, it is passed, nullptr on errors
-        virtual void on_run_end( const run_record* /*rec*/, const dag_node& ){};
+        virtual void on_run_end( const exec_record&, const run_record* /*rec*/, const dag_node& )
+        {
+        }
+
+        virtual void on_exec_end( const exec_record& )
+        {
+        }
 
         virtual ~exec_visitor() = default;
 };
